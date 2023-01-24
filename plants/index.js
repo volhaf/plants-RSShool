2b0f9faea090a3b093dca4c1d18368f976a4d627
VolhaF <Volha.f@outlook.com>
2023-01-24 15:10:59 +0100

    feat: add burger-menu

--- plants/index.js
+++ plants/index.js
@@ -5,3 +5,23 @@ console.log (`Вёрстка валидная +10
 Интерактивность, реализуемая через css +20 
 Итого 110 баллов`)
 
+document.addEventListener('DOMContentLoaded', () => {
+    const hiddenClass = 'navigation-hidden';
+    const burgerBtn = document.getElementById('burgerBtn');
+    const burgerCloseBtn = document.getElementById('navBurgerClose');
+    const navMenu = document.getElementById('navigationMenu');
+    burgerBtn.addEventListener('click', e => {
+        navMenu.classList.remove(hiddenClass);
+    });
+
+    navMenu.addEventListener('click', e => {
+        if (e.target.classList.contains('nav-link')) {
+            navMenu.classList.add(hiddenClass);
+        }
+    });
+
+    burgerCloseBtn.addEventListener('click', e => {
+        e.stopImmediatePropagation();
+        navMenu.classList.add(hiddenClass);
+    });
+});
