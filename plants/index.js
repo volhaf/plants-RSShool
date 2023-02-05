d46a4fbdd57a0e190b3da5324638d5ebe26c08d2
VolhaF <Volha.f@outlook.com>
2023-02-06 00:59:10 +0100

    feat: add to images accordion and section prices

--- plants/index.js
+++ plants/index.js
@@ -1,7 +1,7 @@
-'use strict'
+(function() {
+    'use strict';
 
-
-console.log (`Plants#1
+    console.log (`Plants#1
 Требования к вёрстке:
 1.Вёрстка валидная +10;
 2.Вёрстка семантическая +20;
@@ -23,78 +23,92 @@ Plants#3
 3.В разделе contacts реализован select с выбором городов +25;
 Oценка за задание 100 / 100`);
 
-
-//burger-menu
-document.addEventListener('DOMContentLoaded', () => {
-    const hiddenClass = 'navigation-hidden';
-    const burgerBtn = document.getElementById('burgerBtn');
-    const burgerCloseBtn = document.getElementById('navBurgerClose');
-    const navMenu = document.getElementById('navigationMenu');
-    burgerBtn.addEventListener('click', () => {
-        navMenu.classList.remove(hiddenClass);
-    });
-
-    navMenu.addEventListener('click', e => {
-        if (e.target.classList.contains('nav-link')) {
-            navMenu.classList.add(hiddenClass);
-        }
-    });
-
-    burgerCloseBtn.addEventListener('click', e => {
-        e.stopImmediatePropagation();
-        navMenu.classList.add(hiddenClass);
-    });
-
-    const activeServiceBtnClass = 'button-active';
-    const blurServiceClass = 'blur';
-    const serviceItems = document.querySelectorAll('div.blok-servis-photo div.item-servis');
-    const gardenBtns = document.querySelectorAll('div.btn-servis button.button-item');
-    let selectedServices = new Set();
-    const updateServiceView = () => {
-        serviceItems.forEach(item => 
-            selectedServices.has(item.dataset.service) || selectedServices.size == 0
-                ? item.classList.remove(blurServiceClass)
-                : item.classList.add(blurServiceClass));
-
-        gardenBtns.forEach(btn =>
-            selectedServices.has(btn.dataset.service)
-                ? btn.classList.add(activeServiceBtnClass)
-                : btn.classList.remove(activeServiceBtnClass));
-    };
-    for (let btn of gardenBtns) {
-        btn.addEventListener('click', e => {
-            const buttonService = e.target.dataset.service;
-            selectedServices.has(buttonService) 
-                ? selectedServices.delete(buttonService) 
-                : selectedServices.add(buttonService);
-            selectedServices.size == gardenBtns.length && selectedServices.clear();
-            updateServiceView();
+    document.addEventListener('DOMContentLoaded', () => {
+        const hiddenClass = 'navigation-hidden';
+        const burgerBtn = document.getElementById('burgerBtn');
+        const burgerCloseBtn = document.getElementById('navBurgerClose');
+        const navMenu = document.getElementById('navigationMenu');
+        burgerBtn.addEventListener('click', () => {
+            navMenu.classList.remove(hiddenClass);
         });
-    }
 
-    const addresses = new Map([
-        ['canandaigua', {city: 'Canandaigua, NY', phone: '+1 585 393 0001', office: '151 Charlotte Street'}],
-        ['newyork', {city: 'New York City', phone: '+1 212 456 0002', office: '9 East 91st Street'}],
-        ['yonkers', {city: 'Yonkers, NY', phone: '+1 914 678 0003', office: '511 Warburton Ave'}],
-        ['sherrill', {city: 'Sherrill, NY', phone: '+1 315 908 0004', office: '14 WEST Noyes BLVD'}],
-    ]);
+        navMenu.addEventListener('click', e => {
+            if (e.target.classList.contains('nav-link')) {
+                navMenu.classList.add(hiddenClass);
+            }
+        });
 
-    const activeAddressClass = 'active';
-    const addressInfo = document.getElementById('addressInfo');
-    const addressSelect = document.getElementById('addressSelect');
-    addressSelect.addEventListener('change', event => {
-        const value = event.target.value;
-        if (addresses.has(value)) {
-            const address = addresses.get(value);
-            document.getElementById('addressCity').textContent = address.city;
-            document.getElementById('addressPhone').textContent = address.phone;
-            document.getElementById('addressOffice').textContent = address.office;
-            document.getElementById('callLink').setAttribute('href', `tel:${address.phone}`);
-            addressInfo.classList.add(activeAddressClass);
-            addressSelect.classList.add(activeAddressClass);
-        } else {
-            addressInfo.classList.remove(activeAddressClass);
-            addressSelect.classList.remove(activeAddressClass);
+        burgerCloseBtn.addEventListener('click', e => {
+            e.stopImmediatePropagation();
+            navMenu.classList.add(hiddenClass);
+        });
+
+        const activeServiceBtnClass = 'button-active';
+        const blurServiceClass = 'blur';
+        const serviceItems = document.querySelectorAll('div.blok-servis-photo div.item-servis');
+        const gardenBtns = document.querySelectorAll('div.btn-servis button.button-item');
+        let selectedServices = new Set();
+        const updateServiceView = () => {
+            serviceItems.forEach(item => 
+                selectedServices.has(item.dataset.service) || selectedServices.size == 0
+                    ? item.classList.remove(blurServiceClass)
+                    : item.classList.add(blurServiceClass));
+
+            gardenBtns.forEach(btn =>
+                selectedServices.has(btn.dataset.service)
+                    ? btn.classList.add(activeServiceBtnClass)
+                    : btn.classList.remove(activeServiceBtnClass));
+        };
+        for (let btn of gardenBtns) {
+            btn.addEventListener('click', e => {
+                const buttonService = e.target.dataset.service;
+                selectedServices.has(buttonService) 
+                    ? selectedServices.delete(buttonService) 
+                    : selectedServices.add(buttonService);
+                selectedServices.size == gardenBtns.length && selectedServices.clear();
+                updateServiceView();
+            });
+        }
+
+        const addresses = new Map([
+            ['canandaigua', {city: 'Canandaigua, NY', phone: '+1 585 393 0001', office: '151 Charlotte Street'}],
+            ['newyork', {city: 'New York City', phone: '+1 212 456 0002', office: '9 East 91st Street'}],
+            ['yonkers', {city: 'Yonkers, NY', phone: '+1 914 678 0003', office: '511 Warburton Ave'}],
+            ['sherrill', {city: 'Sherrill, NY', phone: '+1 315 908 0004', office: '14 WEST Noyes BLVD'}],
+        ]);
+
+        const activeAddressClass = 'active';
+        const addressInfo = document.getElementById('addressInfo');
+        const addressSelect = document.getElementById('addressSelect');
+        addressSelect.addEventListener('change', event => {
+            const value = event.target.value;
+            if (addresses.has(value)) {
+                const address = addresses.get(value);
+                document.getElementById('addressCity').textContent = address.city;
+                document.getElementById('addressPhone').textContent = address.phone;
+                document.getElementById('addressOffice').textContent = address.office;
+                document.getElementById('callLink').setAttribute('href', `tel:${address.phone}`);
+                addressInfo.classList.add(activeAddressClass);
+                addressSelect.classList.add(activeAddressClass);
+            } else {
+                addressInfo.classList.remove(activeAddressClass);
+                addressSelect.classList.remove(activeAddressClass);
+            }
+        });
+
+        const shownItemClass = "show";
+        const accordion = document.getElementById('prices');
+        const accordionItems = accordion.querySelectorAll('.accordion-item');
+        for (let btn of accordion.querySelectorAll('button.accordion-button')) {
+            btn.addEventListener('click', e => {
+                const item = e.target.closest('.accordion-item');
+                item.classList.toggle(shownItemClass);
+                accordionItems.forEach(i => {
+                    if (i != item) {
+                        i.classList.remove(shownItemClass);
+                    }
+                });
+            });
         }
     });
-});
+})();
