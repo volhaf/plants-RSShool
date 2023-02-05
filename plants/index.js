2e561b36b65746e5eef1cbbc9069b39ce970908c
VolhaF <Volha.f@outlook.com>
2023-02-05 01:00:39 +0100

    fix: fixed section prices

--- plants/index.js
+++ plants/index.js
@@ -71,4 +71,23 @@ document.addEventListener('DOMContentLoaded', () => {
             updateServiceView();
         });
     }
+
+    const addresses = new Map([
+        ['canandaigua', {city: 'Canandaigua, NY', phone: '+1 585 393 0001', office: '151 Charlotte Street'}],
+        ['newyork', {city: 'New York City', phone: '+1 212 456 0002', office: '9 East 91st Street'}],
+        ['yonkers', {city: 'Yonkers, NY', phone: '+1 914 678 0003', office: '511 Warburton Ave'}],
+        ['sherrill', {city: 'Sherrill, NY', phone: '+1 315 908 0004', office: '14 WEST Noyes BLVD'}],
+    ]);
+
+    const addressSelect = document.getElementById('addressSelect');
+    addressSelect.addEventListener('change', event => {
+        const value = event.target.value;
+        if (addresses.has(value)) {
+            const address = addresses.get(value);
+            document.getElementById('addressCity').textContent = address.city;
+            document.getElementById('addressPhone').textContent = address.phone;
+            document.getElementById('addressOffice').textContent = address.office;
+            document.getElementById('callLink').setAttribute('href', `tel:${address.phone}`);
+        }
+    });
 });
